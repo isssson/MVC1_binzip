d9e66807f6540a9953868e83e8c1263518b746f5
Leejiwon96 <91856738+Leejiwon96@users.noreply.github.com>
2021-10-08 18:03:41 +0900

    Add files via upload

--- 이지원/member/memberDAO.java
+++ 이지원/member/memberDAO.java
@@ -9,25 +9,22 @@ public class memberDAO {
 	private PreparedStatement ps;
 	private ResultSet rs;
 	
-public memberDAO() {
-	System.out.println("DAO테스트");
-}	
-	public void dbConnection() {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url="jdbc:oracle:thin:@localhost:1521:xe";
-			String user="scott";
-			String pwd="1234";
-			conn=DriverManager.getConnection(url,user,pwd);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+	
+	public static final int NOT_ID=1;
+	public static final int NOT_PWD=2;
+	public static final int LOGIN_OK=3;
+	public static final int ERROR=-1;
+	
+	
+	public memberDAO() {
+		System.out.println("DAO테스트");
 	}
+	
 
  public int MemberJoin(memberDTO dto) {
 	 	try {
 			conn=binzip.db.BinzipDB.getConn();
-			String sql="insert into binzip_member values(binzip_member_idx.nextval,?,?,?,?,?,?,?,?,null,sysdate,2)";
+			String sql="insert into binzip_member values(binzip_member_idx.nextval,?,?,?,?,?,?,?,?,'없음',sysdate,2)";
 			ps=conn.prepareStatement(sql);
 			ps.setString(1, dto.getId());
 			ps.setString(2, dto.getPwd());
@@ -98,7 +95,98 @@ public boolean phoneCheck(String userphone) {
 		}
 
 }
-	
+
+//로그인
+public int Logincheck(String userid,String userpwd) {
+	try {
+		conn=binzip.db.BinzipDB.getConn();
+		String sql="select pwd from binzip_member where id=?";
+		ps=conn.prepareStatement(sql);
+		ps.setString(1, userid);
+		rs=ps.executeQuery();
+		if(rs.next()) {
+			String dbpwd=rs.getString(1);
+			if(dbpwd.equals(userpwd)) {
+				return LOGIN_OK;
+			}else {
+				return NOT_PWD;}
+		}else {
+			return NOT_ID;
+		}
+	}catch(Exception e) {
+		e.printStackTrace();
+		return ERROR;
+	}finally {
+		try {
+			if(rs!=null)rs.close();
+			if(ps!=null)rs.close();
+			if(conn!=null)rs.close();
+		}catch(Exception e2) {
+			
+		}
+		}
+}
+
+public String getUserInfo(String userid,String userphone) {
+	try {
+		conn=binzip.db.BinzipDB.getConn();
+		String sql="select phone from binzip_member where id=?";
+		ps=conn.prepareStatement(sql);
+		ps.setString(1, userid);
+		rs=ps.executeQuery();
+		rs.next();
+		return rs.getString(1);
+	}catch(Exception e) {
+		e.printStackTrace();
+		return null;
+	}finally {
+		try {
+			if(rs!=null)rs.close();
+			if(ps!=null)rs.close();
+			if(conn!=null)rs.close();
+		}catch(Exception e2) {
+			
+		}
+		}
+}
+
+
+public ArrayList<memberDTO>memberFind(String fkey,String fvalue){
+	try {
+		conn=binzip.db.BinzipDB.getConn();
+		String sql="select * from binzip_member where"+fkey+"=?";
+		ps=conn.prepareStatement(sql);
+		ps.setString(1, fvalue);
+		rs=ps.executeQuery();
+		ArrayList<memberDTO> arr=new ArrayList<memberDTO>();
+		while(rs.next()) {
+			int idx=rs.getInt("idx");
+			String id=rs.getString("id");
+			String pwd=rs.getString("pwd");
+			String name=rs.getString("name");
+			String email=rs.getString("email");
+			String tel=rs.getString("tel");
+			String addr=rs.getString("addr");
+			java.sql.Date joindate=rs.getDate("joindate");
+			memberDTO dto=new memberDTO(idx, id, pwd, id, pwd, name, tel, tel, email, addr, joindate, idx);
+			arr.add(dto);
+		}
+		return arr;
+	}catch(Exception e) {
+		e.printStackTrace();
+		return null;
+	}finally {
+		try {
+			if(rs!=null)rs.close();
+			if(ps!=null)rs.close();
+			if(conn!=null)rs.close();
+		}catch(Exception e2) {
+			
+		}
+		}
+
+}
+
 	
 
 
